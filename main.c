ca1ca3beda62a5f53691da5bda2baf6b21fbd2ca
juba05 <slimani5792@yahoo.fr>
2017-09-26 00:49:38 +0200

    modification de main.c en ajoutant des commentaires

--- main.c
+++ main.c
@@ -2,10 +2,12 @@
 #include<stdlib.h>
 int main()
 {
-
+// declaration d une variable
 char x;
+//introduire une lettre puis la stocker dans la varible x
 printf("donner une lettre");
 scanf("%c",&x);
+// afficher le resultat
 printf("%c=%d",x,x);
 return 0;
 
